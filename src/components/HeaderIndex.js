edede8d93267fa36d223e859ffb0eb7c8d49f417
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2022-11-28 12:18:26 -0300

    2022.11.28

--- src/components/HeaderIndex.js
+++ src/components/HeaderIndex.js
@@ -6,7 +6,7 @@ const HeaderIndex = () => {
             <div className="grid-header my-4">
                 <div className="my-auto" >
                     <h1 className="header-title light">Wiki<span className='highlight'>Tech</span></h1>
-                    <p className="fs-3 light">Conceptos abarcados: periféricos de <span className="highlight">entrada</span>, <span className="highlight">salida</span>, <span className="highlight">mixtos.</span> y <span className='highlight'> almacenamiento</span></p>
+                    <p className="fs-3 light">Conceptos abarcados: periféricos de <span className="highlight">entrada</span>, <span className="highlight">salida</span>, <span className="highlight">mixtos</span> y <span className='highlight'> almacenamiento.</span></p>
                 </div>
                 <div>
                     <Img src={Peripherals} alt="header" className="img-fluid" />
